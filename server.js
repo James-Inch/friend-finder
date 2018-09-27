c31a33f19d1a03526a1d813941a52c9802ce2715
james inch <james.inch0103@gmail.com>
2018-09-27 17:03:58 -0400

    create home.html

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // npm packages/ dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
+var path = require("path");
 
 // EXPRESS CONFIGURATION
 // This sets up the basic properties for our express server
