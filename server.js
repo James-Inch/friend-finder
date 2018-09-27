909c476c4110ac6002067e044a476b9df83b8896
james inch <james.inch0103@gmail.com>
2018-09-27 19:32:27 -0400

    add htmlRoutes

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 
 // require("./routes/apiRoutes")(app);
-// require("./routes/htmlRoutes")(app);
+require("./routing/htmlRoutes")(app);
 
 // LISTENER
 // The below code effectively "starts" our server
