304131b237240e8e58684b488c715151533f1c15
james inch <james.inch0103@gmail.com>
2018-10-16 21:44:03 -0400

    get vals from survey

--- app/friends.js
+++ app/friends.js
@@ -1,7 +1,9 @@
-const friendArray = [{
-    friendName: "Rick Sanchez",
-    friendpic:  "https://vignette.wikia.nocookie.net/rickandmorty/images/a/a6/Rick_Sanchez.png/revision/latest?cb=20160923150728",
-    friendScore: "50"
-}];
+const friendArray = [
+    {
+        friendName: "Rick Sanchez",
+        friendpic: "https://vignette.wikia.nocookie.net/rickandmorty/images/a/a6/Rick_Sanchez.png/revision/latest?cb=20160923150728",
+        friendScore: "50"
+    }
+];
 
 module.exports = friendArray;
